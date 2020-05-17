6a0d8e9ad5a133ce6c08c7143cf680293353ee5d
eridan123 <98erlen>
2020-05-17 19:11:32 +0600

    some files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ContactComponent} from "./components/contact/contact.component";
+import {ResourcesComponent} from "./components/resources/resources.component";
+import {DocumentComponent} from "./components/document/document.component";
+import {AboutComponent} from "./components/about/about.component";
+import {HomeComponent} from "./components/home/home.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },{
+    path: 'document',
+    component: DocumentComponent,
+  },{
+    path: 'resource',
+    component: ResourcesComponent,
+  },{
+    path: 'contact',
+    component: ContactComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HomeComponent} from "./components/home/home.component";
+import {AboutComponent} from "./components/about/about.component";
+import {DocumentComponent} from "./components/document/document.component";
+import {ContactComponent} from "./components/contact/contact.component";
+import {ResourcesComponent} from "./components/resources/resources.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule } from '@angular/material/input';
+import {HttpClientModule} from "@angular/common/http";
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    DocumentComponent,
+    ContactComponent,
+    ResourcesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatIconModule,
+    MatChipsModule,
+    MatDividerModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatListModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
